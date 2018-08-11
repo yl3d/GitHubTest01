ac83dd3129abe5771f354fcb034dcd6d4714243f
yl3d <yl3dyl3d@vip.sina.com>
2018-08-11 13:11:47 +0800

    fixed中文

--- c01_code/01_helloworld/helloworld.cpp
+++ c01_code/01_helloworld/helloworld.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World! 中文" << endl;
+    cout << " 西文：Hello, World! 中文：你好，世界！" << endl;
+    // chcp 65001（转UTF-8）后，["]后面必先空一格，即现西文空格，否则乱码
     return 0;
 }
